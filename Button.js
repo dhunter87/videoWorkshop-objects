2ce412fec28878002d790b56cd800f63c2bae498
Dan Hunter <dan.hunter55@yahoo.com>
2018-10-23 13:45:50 +0100

    code edits and new files

---
+++ Button.js
@@ -0,0 +1,11 @@
+class Button {
+    constructor(label, x, y){
+        this.label = label
+        this.x = x
+        this.y = y
+        console.log("Button created!")
+        console.log("label : " + label)
+        console.log("x : " + x)
+        console.log("y : " + y)
+    }
+}
